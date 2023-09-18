1b06df137faeb3da8d3875acab784e431d52338e
NikkiG <nlgarlic@temple.edu>
2023-09-18 14:07:37 -0700

    updating

---
+++ tests/testthat/test-lib-summary.R
@@ -0,0 +1,19 @@
+test_that("lib_summary returns expected results", {
+  res <- lib_summary()
+  expect_s3_class(res, "data.frame")
+  expect_equal(ncol(res), 2)
+  expect_equal(names(res), c("Library", "n_packages"))
+  expect_type(res$Library, "character")
+  expect_type(res$n_packages, "integer")
+})
+
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary("foo"), "sizes must be a logical value")
+})
+
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  expect_equal(ncol(res),3)
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  expect_type(res$lib_size, "double")
+})
